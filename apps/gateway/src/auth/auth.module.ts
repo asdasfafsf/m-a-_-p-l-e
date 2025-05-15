bab77ad1d11892d67062d946933443c584bf1814
Wonkeun <31441473+asdasfafsf@users.noreply.github.com>
2025-05-15 15:56:51 +0900

    feat: Jwt 기본 틀 추가

--- apps/gateway/src/auth/auth.module.ts
+++ apps/gateway/src/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RolesGuard } from './roles.guard';
 import { AuthController } from './auth.controller';
-
+import { JwtAuthGuard } from './jwt-auth.guard';
 @Module({
-  providers: [RolesGuard],
+  providers: [JwtAuthGuard, RolesGuard],
   controllers: [AuthController],
+  exports: [JwtAuthGuard, RolesGuard],
 })
 export class AuthModule {}

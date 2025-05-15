0a703c21d2cdc33a0037196266bf16619cfe81d3
Wonkeun <31441473+asdasfafsf@users.noreply.github.com>
2025-05-15 19:34:47 +0900

    feat: set mongo db

--- apps/auth/src/app.module.ts
+++ apps/auth/src/app.module.ts
@@ -5,19 +5,31 @@ import { ConfigModule } from '@nestjs/config';
 import { validationSchema } from './config/validationSchema';
 import { JwtConfig } from './config/jwtConfig';
 import { join } from 'path';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseConfig } from './config/mongooseConfig';
 
 @Module({
   imports: [
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: process.env.MONGO_URI,
+        dbName: process.env.MONGO_DB_NAME,
+      }),
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
       validationSchema,
-      load: [JwtConfig],
+      load: [JwtConfig, MongooseConfig],
       envFilePath: [
         process.env.NODE_ENV === 'local'
           ? join(__dirname, '.env.local')
           : join(__dirname, '.env'),
       ],
     }),
+    AuthModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],

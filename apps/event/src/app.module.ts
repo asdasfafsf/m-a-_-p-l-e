069c9eec6bdafba79aed87d0a963101f5c3661b4
Wonkeun <31441473+asdasfafsf@users.noreply.github.com>
2025-05-18 14:16:51 +0900

    feat: setup config

--- apps/event/src/app.module.ts
+++ apps/event/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { validationSchema } from './config/validationSchema';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema,
+      load: [],
+      envFilePath: [
+        process.env.NODE_ENV === 'local'
+          ? join(__dirname, '.env.local')
+          : join(__dirname, '.env'),
+      ],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

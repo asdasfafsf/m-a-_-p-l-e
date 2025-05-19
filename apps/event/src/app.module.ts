ca1c891a5a7f6a79b9df833902477c0d06eadfeb
Wonkeun <31441473+asdasfafsf@users.noreply.github.com>
2025-05-19 18:55:49 +0900

    feat: add interceptor

--- apps/event/src/app.module.ts
+++ apps/event/src/app.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { APP_FILTER } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { MongooseModule } from '@nestjs/mongoose';
 import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { validationSchema } from './config/validationSchema';
+import { EventRewardHistoryModule } from './event-reward-history/event-reward-history.module';
 import { EventModule } from './event/event.module';
 import { AllExceptionsFilter } from './filters/all-exceptions.filter';
-import { EventRewardHistoryModule } from './event-reward-history/event-reward-history.module';
+import { ApiResponseInterceptor } from './interceptopr/api-response.interceptor';
 
 @Module({
   imports: [
@@ -35,6 +36,7 @@ import { EventRewardHistoryModule } from './event-reward-history/event-reward-hi
   providers: [
     AppService,
     { provide: APP_FILTER, useClass: AllExceptionsFilter },
+    { provide: APP_INTERCEPTOR, useClass: ApiResponseInterceptor },
   ],
 })
 export class AppModule {}
